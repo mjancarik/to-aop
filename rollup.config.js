3d26fcd277d55e64faac5be0b4182d4f9dc4db02
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2020-09-16 22:55:57 +0200

    chore(package): update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,17 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import uglify from 'rollup-plugin-uglify';
-
-const env = process.env.NODE_ENV;
 const config = {
   input: 'src/main.js',
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      externalHelpers: true,
-    }),
-    nodeResolve(),
-    commonjs(),
+  output: [
+    {
+      file: `./dist/toAop.cjs`,
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
+      file: `./dist/toAop.mjs`,
+      format: 'esm',
+      exports: 'named',
+    },
   ],
 };
-
-if (env === 'es' || env === 'cjs') {
-  config.output = { format: env };
-}
-
-if (env === 'umd') {
-  config.output = { format: 'umd', name: 'ConsumeMultipleContexts' };
-
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false,
-      },
-    })
-  );
-}
 
 export default config;
