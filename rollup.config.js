799f63cd7eec1e8a6d2ac91c87679644a8674e28
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2020-09-19 06:24:59 +0200

    fix: added js file extension to dist

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,11 @@ const config = {
   input: 'src/main.js',
   output: [
     {
+      file: `./dist/toAop.js`,
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
       file: `./dist/toAop.cjs`,
       format: 'cjs',
       exports: 'named',
